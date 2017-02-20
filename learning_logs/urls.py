cbef84dfdc6d82e749ce2d7e09d357736b60314e
sfarzy <salmanulfarzy@gmail.com>
2017-02-20 20:09:13 +0530

    add page listing all topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,3 +6,6 @@ from . import views
 urlpatterns = [
         #  Home page
         url(r'^$', views.index, name='index'),
+
+        # Show all topics
+        url(r'^topics/$', views.topics, name='topics'),
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -6,3 +6,9 @@ from .models import Topic
 def index(request):
     """Home page for Learning Log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics' : topics}
+    return render(request, 'learning_logs/topics.html', context)
