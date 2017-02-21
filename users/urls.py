a5ad8cfc43e41e5d94aab5d11bc5d77d1a11d0cd
sfarzy <salmanulfarzy@gmail.com>
2017-02-21 16:54:57 +0530

    Enable user login

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+""" Defines URL patters for users"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import login
+
+
+from . import views
+
+urlpatterns = [
+    url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+]
