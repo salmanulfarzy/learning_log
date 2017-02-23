0f910563e76ecc54e7cfe91b5b10113422dd273a
sfarzy <salmanulfarzy@gmail.com>
2017-02-23 12:29:08 +0530

    Ser homepage for login/other users, local debug
    - Enable local debugging
    - Welcome authenticated users to topic pages and others with home page

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6rt7!o%=d+04jwhdtqk%@$o@y$%qr5cyz0=b&@yinzftb@^%oo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
