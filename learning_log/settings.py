adbae2b5e225f45d169c4b07a2f97c65cf581ae6
sfarzy <salmanulfarzy@gmail.com>
2017-02-27 22:04:24 +0530

    Config static files from Heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -129,6 +129,18 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['sfarzy-learning-log.herokuapp.com']
     DEBUG = False
 
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.9/howto/static-files/
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATIC_URL = '/static/'
+
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
+
     # Static asses configuration
     # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
