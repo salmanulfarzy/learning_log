7d36ecccd5b46250f306eb0fed1b573ac4ee32c2
sfarzy <salmanulfarzy@gmail.com>
2017-02-27 21:06:39 +0530

    Properly set BASE_DIR

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -131,7 +131,7 @@ if os.getcwd() == '/app':
 
     # Static asses configuration
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    STATIC_ROOT = os.path.join('BASE_DIR', 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
