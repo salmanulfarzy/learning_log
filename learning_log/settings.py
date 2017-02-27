f08d83ec204af88d0c0f2337dd17a062a1013278
sfarzy <salmanulfarzy@gmail.com>
2017-02-27 22:13:12 +0530

    Load staticfiles to base.html

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -129,22 +129,10 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['sfarzy-learning-log.herokuapp.com']
     DEBUG = False
 
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/1.9/howto/static-files/
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-    STATIC_URL = '/static/'
-
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-
     # Static asses configuration
-    # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # STATIC_URL = '/static/'
-    # STATICFILES_DIRS = (
-    #     os.path.join(BASE_DIR, 'static'),
-    # )
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
